c0854686fb69a88099f96d768461747f16f29dfc
sonadztux <nanda@anakdesa.id>
2019-12-01 19:26:09 +0700

    Update Config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-telegram_url = 'https://www.telegram.me'
-bot_name = 'CompassBOT'
-token = 'YOUR_BOT_TOKEN_HERE'
-url = f'{telegram_url}/{bot_name}?start={token}'
+url_api_base = 'https://lupabapak.com/sepatu/api.php'
+product_url_base = 'https://sepatucompass.com/collections/all/products/'
+bot_token = '90306XXX:XXXXYDP48jDVUTJ1v5b_lUrkQuCWohiQ'
+chat_id = 'XXXXX'
